29759322a3945a21bda5afcaf3f9d815bfe4e988
safwan <safwaniftekher@gmail.com>
2021-07-29 11:11:18 +0600

    a complete fulstack idea sharing app

---
+++ app.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const session = require("express-session");
+const dotenv = require("dotenv");
+dotenv.config();
+const MongoDbStore = require("connect-mongo");
+const flash = require("connect-flash");
+const markdown = require("marked");
+const csrf = require("csurf");
+const app = express();
+const sanitizeHTML = require("sanitize-html");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api", require("./router-api"));
+
+let sessionOptions = session({
+  secret: "story book",
+  resave: false,
+  saveUninitialized: false,
+  store: MongoDbStore.create({
+    mongoUrl: process.env.CONNECTIONSTRING,
+  }),
+  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
+});
+
+app.use(sessionOptions);
+
+app.use(flash());
+
+app.use(function (req, res, next) {
+  //make our markdown function available from within ejs templates
+  res.locals.filterUserHTML = function (content) {
+    return sanitizeHTML(markdown(content), {
+      allowedTags: [
+        "p",
+        "br",
+        "ul",
+        "li",
+        "ol",
+        "strong",
+        "bold",
+        "i",
+        "em",
+        "h1",
+        "h2",
+        "h3",
+      ],
+      allowedAttributes: {},
+    });
+  };
+
+  //make all errors and success messagges available from all tamplates
+  res.locals.errors = req.flash("errors");
+  res.locals.success = req.flash("success");
+
+  //make current user id available on the req object
+  if (req.session.user) {
+    req.visitorId = req.session.user._id;
+  } else {
+    req.visitorId = 0;
+  }
+
+  //make available user session data available from within view templates
+  res.locals.user = req.session.user;
+  next();
+});
+
+const router = require("./router");
+
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(csrf());
+
+app.use(function (req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use("/", router);
+
+app.use(function (err, req, res, next) {
+  if (err) {
+    if (err.code == "EBADCSRFTOKEN") {
+      req.flash("errors", "Cross site request forgery detected.");
+      req.session.save(() => res.redirect("/"));
+    } else {
+      res.render("404");
+    }
+  }
+});
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+io.use(function (socket, next) {
+  sessionOptions(socket.request, socket.request.res, next);
+});
+
+io.on("connection", function (socket) {
+  if (socket.request.session.user) {
+    let user = socket.request.session.user;
+
+    socket.emit("welcome", { username: user.username, avatar: user.avatar });
+
+    socket.on("chatMessageFromBrowser", function (data) {
+      socket.broadcast.emit("chatMessageFromServer", {
+        message: sanitizeHTML(data.message, {
+          allowedTags: [],
+          allowedAttributes: [],
+        }),
+        username: user.username,
+        avatar: user.avatar,
+      });
+    });
+  }
+});
+
+module.exports = server;
---
+++ router.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("./controllers/userController");
+const postController = require("./controllers/postController");
+const followController = require("./controllers/followController");
+
+//user related route
+router.get("/", userController.home);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.post("/doesUsernameExist", userController.doesUsernameExist);
+router.post("/doesEmailExist", userController.doesEmailExist);
+
+//profile related route
+router.get(
+  "/profile/:username",
+  userController.ifUserExists,
+  userController.sharedProfileData,
+  userController.profilePostsScreen
+);
+
+router.get(
+  "/profile/:username/followers",
+  userController.ifUserExists,
+  userController.sharedProfileData,
+  userController.profileFollowersScreen
+);
+router.get(
+  "/profile/:username/following",
+  userController.ifUserExists,
+  userController.sharedProfileData,
+  userController.profileFollowingScreen
+);
+
+//find people related route
+router.get("/userlist", userController.mustBeLoggedIn, userController.userlist);
+
+//post/status related rout
+router.get(
+  "/create-post",
+  userController.mustBeLoggedIn,
+  postController.viewCreateScreen
+);
+router.post(
+  "/create-post",
+  userController.mustBeLoggedIn,
+  postController.create
+);
+router.get("/post/:id", postController.viewSingle);
+router.get(
+  "/post/:id/edit",
+  userController.mustBeLoggedIn,
+  postController.viewEditScreen
+);
+router.post(
+  "/post/:id/edit",
+  userController.mustBeLoggedIn,
+  postController.edit
+);
+
+router.post(
+  "/post/:id/delete",
+  userController.mustBeLoggedIn,
+  postController.delete
+);
+
+router.post("/search", postController.search);
+
+//follow related route
+router.post(
+  "/addFollow/:username",
+  userController.mustBeLoggedIn,
+  followController.addFollow
+);
+router.post(
+  "/removeFollow/:username",
+  userController.mustBeLoggedIn,
+  followController.removeFollow
+);
+module.exports = router;
